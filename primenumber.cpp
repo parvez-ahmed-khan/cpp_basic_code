a8e0286dc1252d6156991b2e87f8472770e07846
parvezkhan11bd <engparvezkhan.pk@gmail.com>
2025-12-04 11:13:15 +0600

    a

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,22 +1,31 @@
 #include<iostream>
-#include <cmath>
+#include<cmath>
 using namespace std;
 int main(){
-    int num, flag=0, i;
-    cout<<"Enter a number:";
-    cin>>num;
+    int startingnum, endingnum, flag=0, count=0,sum=0;
+    cout<<"Enter Starting Number:";
+    cin>>startingnum;
+    cout<<"Enter Ending Number:";
+    cin>>endingnum;
+    for(int num = startingnum; num <= endingnum; num++) {
     if(num<=1){
         flag=1;
-    }
-    for(i=2; i<= sqrt(num); i++){
-        if(num%i == 0){
+    }else{
+        flag = 0;
+        for(int i=2; i<=sqrt(num); i++){
+            if(num%i==0){
             flag=1;
             break;
+            }
+            }
+         }if(flag == 0) {
+            sum=sum+num;
+            cout <<num<<endl;
+            count++;
+            
         }
+        
     }
-    if(flag==0){
-        cout<<num<<" is a prime number"<<endl;
-    }else{
-        cout<<num<<" is not a prime number"<<endl;
-    }
+    cout << "Total Prime Numbers = " << count<<endl;
+    cout << "Total Sum of Numbers = " << sum;
 }
